a84d78c792a7cf5a56bfb4a3979244026b2d13f1
Satwant <satwantsingh9451@gmail.com>
2023-12-26 20:04:43 +0530

    ceaser cipher project added

--- functions/ceaser cipher proj/main_.py
+++ functions/ceaser cipher proj/main_.py
@@ -1,18 +1,36 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-def encrypt(text_1, shift_1):
-    encrypt_word = ''
-    for x in text_1:
-        positio_n = alphabet.index(x)
-        new_position = positio_n+shift_1
-        encrypt_word += (alphabet[new_position])
-    print(f"The encoded text is {encrypt_word}")
-
-encrypt(text_1=text, shift_1=shift)
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
+    print(f"Here's the {cipher_direction}d result: {end_text}")
 
+from art import logo
+
+print(logo)
+
+should_end = False
+while not should_end:
+
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift % 26
+
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+    restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+    if restart == "no":
+        should_end = True
+        print("Goodbye")
--- functions/function_prctice.py
+++ functions/function_prctice.py
@@ -2,3 +2,11 @@ def greet(name, location):
     print(f"Welcome {name}")
     print(f"to  {location}")
 greet(name = 'harry', location='UK')
+
+def repeat_iterable(a, N):
+    factor = int(N / len(a) + 1)
+    repeated_list = a * factor
+    print(repeated_list[:N])
+repeat_iterable([1,2,3],10)
+
+
